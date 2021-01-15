147875f5dbc34106ff1544099603f76f34eb9214
Renata Guedes <renataleitesg@gmail.com>
2021-01-15 15:09:15 +0000

    implents the DELETE method

--- api/controllers/toDoList.js
+++ api/controllers/toDoList.js
@@ -7,9 +7,9 @@ module.exports = app => {
   const {
     toDoList: toDoListMock,
   } = toDoListDB;
-  controller.listToDo = (req, res) => res.status(200).json(toDoListDB);
+  controller.listTask = (req, res) => res.status(200).json(toDoListDB);
 
-  controller.saveToDo = (req, res) => {
+  controller.saveTask = (req, res) => {
     toDoListMock.data.push({
       id: uuidv4(),
       task: req.body.task,
@@ -18,5 +18,26 @@ module.exports = app => {
 
     res.status(201).json(toDoListMock);
   }
+  controller.removeTask = (req, res) => {
+    const {
+      taskId,
+    } = req.params;
+
+    const foundTaskIndex = toDoListMock.data.findIndex(task => task.id === taskId);
+    if (foundTaskIndex ===-1) {
+      res.status(404).json({
+        message: "Tarefa não encontrada na base de dados.",
+        sucess: false,
+        toDoList: toDoListMock,
+      });
+    } else {
+      toDoListMock.data.splice(foundTaskIndex, 1);
+      res.status(200).json({
+        message: "Tarefa encontrada e deletada com sucesso.",
+        sucess: true,
+        toDoList: toDoListMock,
+      });
+    }
+  };
   return controller;
 }
--- api/routes/toDoList.js
+++ api/routes/toDoList.js
@@ -2,6 +2,9 @@ module.exports = app => {
   const controller = app.controllers.toDoList;
   
   app.route('/api/v1/todolist')
-    .get(controller.listToDo)
-    .post(controller.saveToDo);
+    .get(controller.listTask)
+    .post(controller.saveTask);
+  
+  app.route('/api/v1/todolist/:taskId')
+      .delete(controller.removeTask);
 }
