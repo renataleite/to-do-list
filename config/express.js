d472824243a25165c6337e3b43b0929b992dc644
Renata Guedes <renataleitesg@gmail.com>
2021-01-19 22:51:40 +0000

    change Access-Control-Allow-Origin

--- config/express.js
+++ config/express.js
@@ -13,7 +13,7 @@ module.exports = () => {
   app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://nostalgic-pike-cbefd5.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
