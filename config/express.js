efb763264892d09ed02e6888ffff4899d5a93a72
Renata Guedes <renataleitesg@gmail.com>
2021-02-02 23:21:06 +0000

    remove mock file and change CORS policy

--- config/express.js
+++ config/express.js
@@ -26,7 +26,6 @@ module.exports = () => {
   // MIDDLEWARES
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://to-do-list-renata.netlify.app');
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -36,7 +35,6 @@ module.exports = () => {
 
   //ENDPOINTS
   consign({ cwd: 'api' })
-    .then('data')
     .then('controllers')
     .then('routes')
     .into(app);
